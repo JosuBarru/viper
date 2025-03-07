062c1a323104a76a239f6b48661d3dc71a0b7d41
Josu Barrutia <jbarrutia006@gmail.com>
2025-03-07 18:58:24 +0100

    track large files

--- scripts/dpotrain.py
+++ scripts/dpotrain.py
@@ -54,9 +54,9 @@ def train_dpo(args):
     print("Loading model and tokenizer...")
 
 
-    max_seq_length = 4096 # Choose any! We auto support RoPE Scaling internally!
+    #max_seq_length = 4096 # Choose any! We auto support RoPE Scaling internally!
     dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+    #load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
     model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = args.model_name,
